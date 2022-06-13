b7c294429c27726214c3b8ae8a9152364db16b82
ivanlbev <jean.cygne@me.com>
2022-06-13 18:28:22 +0300

    links и birthdays отдают данные

--- app.py
+++ app.py
@@ -1,14 +1,40 @@
 from components.xml_reader import *
-from flask import Flask
+from flask import Flask, jsonify
+
+data = XmlReader('data_xml/Сотрудники.xml')
+data.get_actual_birth()
+
+domen = 'http://127.0.0.1:5000'
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
 
+@app.route("/birthdays/")
+def get_persons():
+    serialaze_dict = {}
+    for index,value in enumerate(data.get_persons_list()):
+        serialaze_dict[index] = value
+    print(serialaze_dict)
+    return serialaze_dict
 
-if __name__ == '__main__':
-    data = XmlReader('data_xml/Сотрудники.xml')
-    data.get_actual_birth()
-    print(data.get_persons_list())
+@app.route("/links/")
+def get_links():
+    links_dict = {}
+    for index, value in enumerate(data.get_persons_list()):
+        alias = value['id'][1:-1]
+        links_dict[index] = domen + '/links/' + alias
+    return links_dict
+
+@app.route("/birthdays/<id>")
+def hello_world3(id):
+    return f"<p>Hello, World!{id}</p>"
+
+
+# if __name__ == '__main__':
+#     data = XmlReader('data_xml/Сотрудники.xml')
+#     data.get_actual_birth()
+#     print(data.get_persons_list())
