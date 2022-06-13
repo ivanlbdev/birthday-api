2e23e4c142c1077982d94e4620f43d0ee262025a
ivanlbev <jean.cygne@me.com>
2022-06-13 17:52:11 +0300

    добавили flask

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
 from components.xml_reader import *
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
 
 
 if __name__ == '__main__':
